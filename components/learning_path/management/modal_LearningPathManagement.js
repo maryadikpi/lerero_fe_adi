13354172ee8d4fb3971244eb091a29de21068c34
Chalid Ade Rahman <chalidade@gmail.com>
2020-09-25 11:55:35 +0700

    [Learning Path Management] Add New Page

---
+++ components/learning_path/management/modal_LearningPathManagement.js
@@ -0,0 +1,231 @@
+const ModalLearningPathMenagement = () => (
+  <>
+    <div
+      className="modal fade"
+      id="addGroup"
+      tabIndex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog text-dark" role="document">
+        <div className="modal-content">
+          <div className="modal-header text-center">
+            <h5 className="modal-title width-100">Create User Group</h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body p-3">
+            <div className="row align-center p-3">
+              <div className="col-12">
+                <label htmlFor="groupName" className="container-fluid  p-2">
+                  User Group's Name
+                </label>
+                <input
+                  id="groupName"
+                  type="text"
+                  className="form-control border-top-0 border-right-0 border-left-0"
+                  placeholder="Type user's group name"
+                />
+              </div>
+            </div>
+            <br />
+
+            <div className="row mb-5">
+              <div className="col-6">
+                <button
+                  type="button"
+                  className="btn btn-sm btn-danger width-90 float-right"
+                  data-dismiss="modal"
+                >
+                  Cancel
+                </button>
+              </div>
+              <div className="col-6">
+                <button
+                  type="button"
+                  className="btn width-90 btn-sm btn-primary"
+                >
+                  Create User Group
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      className="modal fade"
+      id="deactiveLearningPath"
+      tabIndex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog text-dark" role="document">
+        <div className="modal-content">
+          <div className="modal-header text-center">
+            <h5 className="modal-title width-100">Warning!</h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body p-3">
+            <div className="row align-center p-3">
+              <div className="col-12 text-center">
+                <p>
+                  <i className="fa fa-exclamation-triangle text-warning icon-width-50"></i>
+                </p>
+                <p>Are you sure want to Deactive this badge : </p>
+              </div>
+            </div>
+            <br />
+
+            <div className="row mb-5">
+              <div className="col-6">
+                <button
+                  type="button"
+                  className="btn btn-sm btn-danger width-90 float-right"
+                  data-dismiss="modal"
+                >
+                  Cancel
+                </button>
+              </div>
+              <div className="col-6">
+                <button
+                  type="button"
+                  data-dismiss="modal"
+                  className="btn width-90 btn-sm btn-primary"
+                >
+                  Ok
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      className="modal fade"
+      id="deleteLearningPath"
+      tabIndex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog text-dark" role="document">
+        <div className="modal-content">
+          <div className="modal-header text-center">
+            <h5 className="modal-title width-100">Warning!</h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body p-3">
+            <div className="row align-center p-3">
+              <div className="col-12 text-center">
+                <p>
+                  <i className="fa fa-exclamation-triangle text-danger icon-width-50"></i>
+                </p>
+                <p>Are you sure want to Delete this badge ? </p>
+                <p>Please note that this process cannot be reversed </p>
+              </div>
+            </div>
+            <br />
+
+            <div className="row mb-5">
+              <div className="col-6">
+                <button
+                  type="button"
+                  className="btn btn-sm btn-danger width-90 float-right"
+                  data-dismiss="modal"
+                >
+                  Cancel
+                </button>
+              </div>
+              <div className="col-6">
+                <button
+                  type="button"
+                  data-dismiss="modal"
+                  className="btn width-90 btn-sm btn-primary"
+                >
+                  Ok
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      className="modal fade"
+      id="actionMessage"
+      tabIndex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog text-dark" role="document">
+        <div className="modal-content">
+          <div className="modal-header text-center">
+            <h5 className="modal-title width-100">Warning!</h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body p-3">
+            <div className="row align-center p-3">
+              <div className="col-12 text-center">
+                <p>
+                  <i className="fa fa-check-circle text-success icon-width-50"></i>
+                </p>
+                <p>Success Deative User Group</p>
+                <p>Sales</p>
+                <p>#1</p>
+              </div>
+            </div>
+            <br />
+
+            <div className="row mb-5">
+              <div className="col-12 text-center">
+                <button
+                  type="button"
+                  data-dismiss="modal"
+                  className="btn width-30 btn-sm btn-primary"
+                >
+                  Ok
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+);
+
+export default ModalLearningPathMenagement;
