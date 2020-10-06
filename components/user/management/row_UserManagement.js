8ddb776acaf499651d57ad9c142269026eff605a
maryadikpi <maryadi@kpiinstitute.org>
2020-10-06 10:58:15 +0700

    finishing userRole conversion to reactComponent

--- components/user/management/row_UserManagement.js
+++ components/user/management/row_UserManagement.js
@@ -3,20 +3,45 @@ import BlockUserButton from '../modal/modalBlockUser'
 export default function rowUserManagement (props) {
     const data = props.data
     
+    const [actionType, setActionType] = useState('nothing')
     const [status, setStatus] = useState(data.status)
+    const [newRole, setNewRole] = useState()
+
     var roleList = []
     if (props.roleList.data) {
         roleList = props.roleList.data.map((item, i) => {
-                            return <option key={i} value={item.id}>{item.display_name}</option>
-                        })
+                    return <option key={i} value={item.id}>{item.display_name}</option>
+                })
     }
     
     function handleDelete() {
+        setActionType('delete_user')
         props.setDeleteData({id: data.id, username: data.username})
     }
 
+    function handleSelectRole(event) {
+        setActionType('change_role')
+        let index = event.nativeEvent.target.selectedIndex;
+        setNewRole(event.target.value)
+        props.setUserRoleData({
+            user: data,
+            newRoleId: event.target.value,
+            newRoleText: event.nativeEvent.target[index].text
+        })
+        
+    }
+
     useEffect(() => {
-        $('#deleteUserModal_'+data.id).modal('show')
+        switch(actionType) {
+            case 'delete_user': {
+                $('#deleteUserModal_'+data.id).modal('show') 
+                break
+            }
+            case 'change_role': {
+                $('#changeRoleUserModal_'+data.id).modal('show') 
+                break
+            }
+        }
     })
 
     return (
@@ -54,17 +79,19 @@ export default function rowUserManagement (props) {
                 </button>
 
                 <button
-                type="button"
-                data-toggle="modal"
-                data-target="#changeEmail"
-                className="btn btn-primary width-110px btn-sm m-1"
+                    type="button"
+                    data-toggle="modal"
+                    data-target="#changeEmail"
+                    className="btn btn-primary width-110px btn-sm m-1"
                 >
-                Change Email
+                    Change Email
                 </button>
 
                 <select
                     type="button"
                     className="btn btn-warning width-110px btn-sm m-1"
+                    onChange={handleSelectRole}
+                    // value={role}
                 >
                     {roleList}
                 </select>
---
+++ components/user/modal/modalChangeUserRole.js
@@ -0,0 +1,128 @@
+import {useState, useEffect} from 'react'
+import {Spinner} from "react-bootstrap"
+
+import {kpiFetch} from 'kpi_helper'
+import {BLOCK_USER} from 'config/const_api_url'
+
+export default function modalChangeUserRole(props) {
+    console.log(props)
+    const dataTarget = "#changeRoleUserModal_"+props.userData.user.id
+    const modalId = "changeRoleUserModal_"+props.userData.user.id
+    return(
+        <>
+            <div
+                className="modal fade"
+                id={modalId}
+                tabIndex="-1"
+                role="dialog"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
+                <div className="modal-dialog text-dark" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header text-center">
+                            <h5 className="modal-title width-100">Warning!</h5>
+                            <button
+                            type="button"
+                            className="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                            >
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body p-3">
+                            <div className="row align-center p-3">
+                                <div className="col-12 text-center">
+                                    <p>
+                                    <i className="fa fa-exclamation-triangle text-warning icon-width-50"></i>
+                                    </p>
+                                    <p>Are you sure want to Change Role for User : </p>
+                                    <p>{props.userData.user.first_name} {props.userData.user.last_name}</p>
+                                    <p>into role : </p>
+                                    <p>{props.userData.newRoleText}</p>
+                                </div>
+                            </div>
+                            <br />
+
+                            <div className="row mb-5">
+                                <div className="col-6">
+                                    <button
+                                    type="button"
+                                    className="btn btn-sm btn-danger width-90 float-right"
+                                    data-dismiss="modal"
+                                    >
+                                    Cancel
+                                    </button>
+                                </div>
+                                <div className="col-6">
+                                    <button
+                                    type="button"
+                                    data-dismiss="modal"
+                                    data-toggle="modal"
+                                    data-target="#changeSuccess"
+                                    className="btn width-90 btn-sm btn-primary"
+                                    >
+                                    Ok
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+            <div
+                className="modal fade"
+                id="changeSuccess"
+                tabIndex="-1"
+                role="dialog"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
+                <div className="modal-dialog text-dark" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header text-center">
+                            <h5 className="modal-title width-100">Success!</h5>
+                            <button
+                                type="button"
+                                className="close"
+                                data-dismiss="modal"
+                                aria-label="Close"
+                            >
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body p-3">
+                        <div className="row align-center p-3">
+                        <div className="col-12 text-center">
+                            <p>
+                                <i className="fa fa-check-circle text-success icon-width-50"></i>
+                            </p>
+                            <p>Success Change Role for User</p>
+                            <p>Makaryo Sregep</p>
+                            <p>#251</p>
+                            <p>Into Role</p>
+                            <p>Client Admin</p>
+                        </div>
+                        </div>
+                        <br />
+
+                        <div className="row mb-5">
+                            <div className="col-12 text-center">
+                                <button
+                                    type="button"
+                                    data-dismiss="modal"
+                                    className="btn width-30 btn-sm btn-primary"
+                                >
+                                    Ok
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
